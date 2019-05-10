2ef6ef045ce5416df8db04a6458451b6daae14f1
anjali-rao <raoanjali0211@gmail.com>
2019-05-10 23:39:18 +0530

    adding comments

--- app/views.py
+++ app/views.py
@@ -1,18 +1,24 @@
 import pandas as pd
-import csv
-from collections import defaultdict
 
 from django.shortcuts import render
 from django.http.response import JsonResponse
 from django.template import loader
 
 def select_data(request):
+    '''
+    Renders page to select csv data file name and required fields
+    '''
     filenames = ['file1.csv', 'file2.csv', 'file3.csv']
     fields = ['Sentiment', 'SentimentSource', 'SentimentText']
     context = dict(filenames=filenames, fields=fields)
     return render(request, 'select_data.html', context)
 
 def get_data(request):
+    '''
+    On receiving the filename and fields from user,
+    the appropriate data is selected from the data files
+    available and provided to the user.
+    '''
     filename = request.POST.get('filename', '')
     fields = request.POST.getlist('fields[]', '')
     csv_data = []
@@ -20,12 +26,9 @@ def get_data(request):
         reader = csv.DictReader(f)
         for row in reader:
             csv_data.append([row[i] for i in fields])
-    f.close()
 
     template = loader.get_template('csv_data.html')
     context = dict(csv_data=csv_data, fields=fields, success=True)
     html = template.render(context, request)
-
-    columns = defaultdict(list) # each value in each column is appended to a list
 
     return JsonResponse(dict(html=html, success=True))
